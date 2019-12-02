f0cb6458723a356ebd0284d4c41d1a09767a42ef
Shashank Rajesh <shashank@Shashanks-MacBook-Pro.local>
2019-12-02 16:24:14 -0500

    Webpack config

---
+++ webpack.common.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, 'src/main.js'),
+  module: {
+    rules: [
+      {test: /\.js$/, use: 'babel-loader'},
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+};
