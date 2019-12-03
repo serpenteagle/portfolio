7f12e4f1b20e7239f99974d1bb658f8a78584167
Shashank Rajesh <shashank@Shashanks-MacBook-Pro.local>
2019-12-03 16:20:18 -0500

    Added grid and shade for future development

--- src/App.js
+++ src/App.js
@@ -2,20 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import styled from 'styled-components';
 
+import GridItem from './components/GridItem/GridItem.js';
+
 const Container = styled.div``;
 const Border = styled.div`
-  border: 19px solid black;
-  margin: 35px;
+  border: 16px solid black;
+  margin: 26px;
   border-radius: 19px;
   text-align: center;
 `;
-const Filler = styled.div`
-  height: 1000px;
-  width: 100%;
+const Shade = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: black;
+  transition: opacity 0.5s;
+  opacity: ${({on}) => (on ? '.75' : '0')};
+  z-index: ${({on}) => (on ? 1000 : 'initial')};
+  pointer-events: none;
 `;
 const Title = styled.h1`
   font-family: 'Space Grotesk';
-  font-size: 106px;
+  font-size: 81px;
   font-weight: bold;
   color: black;
 `;
@@ -31,7 +41,7 @@ const Body = styled.p`
   text-align: center;
   width: 1000px;
   font-family: 'Space Grotesk';
-  font-size: 29px;
+  font-size: 22px;
   font-weight: regular;
 `;
 
@@ -40,9 +50,25 @@ const Divider = styled.hr`
   border: 1px solid black;
 `;
 
+const GridWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr;
+  grid-gap: 10px;
+  height: 900px;
+  width: 900px;
+  background-color: blue;
+`;
+
 const App = props => {
   return (
     <Container>
+      <Shade />
       <Border>
         <Title>
           <Highlight>Shashank Rajesh</Highlight>
@@ -53,8 +79,25 @@ const App = props => {
           engineering. Having a keen attention to detail and an obsession with
           aesthetics, something something, something something.
         </Body>
+
+        <GridWrapper>
+          <Grid>
+            <GridItem />
+
+            <GridItem />
+            <GridItem />
+            <GridItem />
+            <GridItem />
+            <GridItem />
+            <GridItem gc="1 / span 2" gr="3 / span 1" />
+            <GridItem />
+          </Grid>
+        </GridWrapper>
+
         <Divider />
-        <Body>ShashankRajesh7@gmail.com &nbsp;&nbsp;&nbsp;&nbsp; 248-567-9425</Body>
+        <Body>
+          ShashankRajesh7@gmail.com &nbsp;&nbsp;&nbsp;&nbsp; 248-567-9425
+        </Body>
       </Border>
     </Container>
   );
