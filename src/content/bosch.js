0e6d0b2d29fda9eaf782c9c54840ac347803b0ff
Shashank Rajesh <shashank@Shashanks-MacBook-Pro.local>
2019-12-09 23:29:17 -0500

    Exports content object with all project content needed for template

---
+++ src/content/bosch.js
@@ -0,0 +1,16 @@
+import bigPic from '../assets/holo-render.jpg';
+import galleryIcon from '../assets/gallery-icon.svg';
+
+export default {
+  title: 'Holonomic Robot',
+  summary: 'Lorem ipsum bruh',
+  bigPic: {src: bigPic},
+  slices: [
+    {src: bigPic, text: 'Lorem ipsum'},
+    {src: bigPic, text: 'Lorem ipsum'},
+  ],
+  links: [
+    {text: 'See Photos', src: galleryIcon},
+    {text: 'Photo Gallery', src: galleryIcon},
+  ],
+};
