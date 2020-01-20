a2a21b7d9fe27966d6399e24bc73668e77e5d62a
Shashank Rajesh <shashank@MacBook-Pro.local>
2020-01-20 16:46:22 -0500

    Webpack config for the sandbox

---
+++ webpack.sandbox.js
@@ -0,0 +1,20 @@
+const common = require('./webpack.common.js');
+const merge = require('webpack-merge');
+const path = require('path');
+const webpack = require('webpack');
+
+const sandbox = {
+  mode: 'development',
+  output: {
+    path: path.join(__dirname, 'sandbox'),
+    filename: 'bundle.js',
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  devServer: {
+    contentBase: path.join(__dirname, 'sandbox'),
+    port: 9000,
+    hot: true,
+  },
+};
+
+module.exports = merge(common, sandbox);
