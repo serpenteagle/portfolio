7d5809d40b8ebfe13a5f972ac366487a85f3032c
Shashank Rajesh <shashank@MacBook-Pro.local>
2020-01-23 11:15:57 -0500

    Updated for development of the home page.

--- src/SandboxApp.js
+++ src/SandboxApp.js
@@ -11,6 +11,9 @@ import BoschHolo from "./views/BoschHolo";
 
 import image from "./assets/holo-render.jpg";
 import ImageWithCaption from "./components/ImageWithCaption/ImageWithCaption.js";
+import ProjectGrid from "./components/ProjectGrid/ProjectGrid.js";
+import ProjectGridItem from "./components/ProjectGrid/ProjectGridItem.js";
+import Typography from "./components/Typography/Typography.js";
 
 const theme = {
   h1: {
@@ -25,32 +28,57 @@ const theme = {
   }
 };
 
+const ProjectImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  filter: brightness(100%) grayscale(25%);
+  transition: all 0.5s;
+`;
+
 const SandboxApp = props => {
   console.log(props.currentBreakpoint);
   return (
+    // <ThemeProvider theme={theme}>
+    //   <BoschHolo />
+    // </ThemeProvider>
+
     <ThemeProvider theme={theme}>
-      {/* <View mobile={props.currentBreakpoint === "mobile"} onBack>
+      <View>
         <Article>
           <ArticleSection>
-            <ImageWithText src={image} caption="This is an image" flipped>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Morbi
-              non arcu risus quis varius quam quisque id. Nunc scelerisque
-              viverra mauris in. Quam viverra orci sagittis eu volutpat odio
-              facilisis.
-            </ImageWithText>
-            <ImageWithText src={image} caption="This is an image">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Morbi
-              non arcu risus quis varius quam quisque id. Nunc scelerisque
-              viverra mauris in. Quam viverra orci sagittis eu volutpat odio
-              facilisis.
-            </ImageWithText>
-            <ImageWithCaption src={image} caption="Caption" />
+            <Typography variant="h1">Shashank Rajesh</Typography>
+            <Typography variant="p" style={{textAlign: 'justify'}}>
+              Self taught front-end developer with a background in robotics
+              engineering. Having a keen attention to detail and an obsession
+              with aesthetics, something something, something something.
+            </Typography>
+            <ProjectGrid>
+              <ProjectGridItem span="2" title="Bosch Holo">
+                <ProjectImage src={image} />
+              </ProjectGridItem>
+              <ProjectGridItem>
+                <ProjectImage src={image} />
+              </ProjectGridItem>
+              <ProjectGridItem>
+                <ProjectImage src={image} />
+              </ProjectGridItem>
+              <ProjectGridItem span="2">
+                <ProjectImage src={image} />
+              </ProjectGridItem>
+              <ProjectGridItem>
+                <ProjectImage src={image} />
+              </ProjectGridItem>
+              <ProjectGridItem>
+                <ProjectImage src={image} />
+              </ProjectGridItem>
+              <ProjectGridItem>
+                <ProjectImage src={image} />
+              </ProjectGridItem>
+            </ProjectGrid>
           </ArticleSection>
         </Article>
-      </View> */}
-      <BoschHolo />
+      </View>
     </ThemeProvider>
   );
 };
