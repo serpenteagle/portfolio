2150d14b3cc57ee88df9969f7f9f488bbd5bcd6e
Shashank Rajesh <shashank@MacBook-Pro.local>
2020-01-28 08:27:19 -0500

    Portfolio page mostly done

--- src/views/Portfolio.js
+++ src/views/Portfolio.js
@@ -9,8 +9,12 @@ import Typography from "../components/Typography/Typography";
 import IconLink from "../components/IconLink/IconLink";
 
 import portfolioDev from "../assets/portfolio-dev.jpg";
+import figma from "../assets/figma.jpg";
+import appCode from "../assets/app-code.jpg";
+import addressBar from "../assets/url.jpg";
+import figmaComponent from "../assets/figma-component.jpg";
 
-import galleryIcon from "../assets/gallery-icon.svg";
+import octocat from "../assets/octocat.svg";
 
 const Portfolio = props => {
   return (
@@ -20,30 +24,37 @@ const Portfolio = props => {
           <Typography variant="h1">Portfolio Website</Typography>
           <Typography variant="p" style={{ textAlign: "justify" }}>
             shashankrajesh.com was created to showcase my various projects
-            online.
+            online. Using an SPA style app architecture, I was able to
+            streamline the development process by utilizing a themable and
+            composable component architecture. Route animations allow for an
+            integrated and seamless experience.
           </Typography>
-          <ImageWithCaption src={portfolioDev} caption="Various projects" />
+          <ImageWithCaption src={portfolioDev} caption="Site with dev tools" />
         </ArticleSection>
         <ArticleSection>
-          <ImageWithText src={portfolioDev} caption="Completed gearbox">
+          <ImageWithText src={figma} caption="Figma designs">
             The design of the site was developed in Figma, a UI design and
             prototyping app. I spent quite a while tweaking the design to create
             an aesthetic that is simple yet bold and sophisticated.
           </ImageWithText>
-          <ImageWithText src={portfolioDev} caption="Intake system" flipped>
+          <ImageWithText src={appCode} caption="React code" flipped>
             The design is implemented in the frontend using React. A component
             based architecture makes it easy to create reusable, composable
             components that can be used throughout the application. In addition,
             styled-components allows for advanced logic based styling as well as
             seamless whole-app themeing.
           </ImageWithText>
-          <ImageWithText src={portfolioDev} caption="2 speed PTO gearbox">
+          <ImageWithText src={addressBar} caption="Address bar">
             react-router is used to control the routing of the site. With its
             capabilities, I was able to create seamless and intuitive page
             transitions that eliminate the feeling of separation between
             destinations.
           </ImageWithText>
-          <ImageWithText src={portfolioDev} caption="Fuel blaster" flipped>
+          <ImageWithText
+            src={figmaComponent}
+            caption="Templating architecture"
+            flipped
+          >
             Each project page is created using an organized yet flexible
             template component. Blocks of content can be created as modules of
             the page, and the template compoenents automatically apply the
@@ -52,10 +63,10 @@ const Portfolio = props => {
         </ArticleSection>
         <ArticleSection style={{ display: "flex", justifyContent: "center" }}>
           <IconLink
-            src={galleryIcon}
-            href="https://photos.app.goo.gl/3c6U1b9Eg7QZYMPd9"
+            src={octocat}
+            href="https://www.github.com/serpenteagle/portfolio"
           >
-            SEE GALLERY
+            SOURCE CODE
           </IconLink>
         </ArticleSection>
       </Article>
